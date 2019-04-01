147b866d1318e7181ab61fc3c296954a9c93193b
= <=>
2019-04-02 01:25:01 +0700

    custom settings fragment

--- app/src/main/java/com/application/tuanlv/comicapp/fragment/SettingsFragment.java
+++ app/src/main/java/com/application/tuanlv/comicapp/fragment/SettingsFragment.java
@@ -11,13 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.application.tuanlv.comicapp.R;
 
-public class SettingsFragment extends Fragment {
-    TextView tvUserName, tvEmail, tvChangePass;
-    TextView tvAboutUs, tvFeedback, tvRateApp;
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+    RelativeLayout tvUserName, tvChangePass, tvLogout;
+    RelativeLayout tvAboutUs, tvFeedback, tvRateApp;
     ImageView imgAvatar;
     Toolbar toolbar;
     public static SettingsFragment newInstance() {
@@ -31,19 +33,30 @@ public class SettingsFragment extends Fragment {
         View view = inflater.inflate(R.layout.settings_fragment, container, false);
         initView(view);
 
+        tvUserName.setOnClickListener(this);
+        tvChangePass.setOnClickListener(this);
+        tvAboutUs.setOnClickListener(this);
+        tvFeedback.setOnClickListener(this);
+        tvRateApp.setOnClickListener(this);
+        tvLogout.setOnClickListener(this);
+
         return view;
     }
 
     private void initView(View view) {
         tvRateApp = view.findViewById(R.id.tv_rate_app_setting);
-        tvUserName = view.findViewById(R.id.tv_username_setting);
-        tvEmail = view.findViewById(R.id.tv_email_setting);
+        tvUserName = view.findViewById(R.id.tv_change_username);
         tvChangePass = view.findViewById(R.id.tv_change_passwd);
         tvAboutUs = view.findViewById(R.id.tv_about_us_setting);
         tvFeedback = view.findViewById(R.id.tv_feed_back_setting);
+        tvLogout = view.findViewById(R.id.tv_logout_setting);
         imgAvatar = view.findViewById(R.id.img_avatar_setting);
         toolbar = view.findViewById(R.id.toolbar_settings);
         ((AppCompatActivity)getActivity()).getSupportActionBar();
-        toolbar.setTitle("Settings");
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getContext(), "Compoinent is clicked", Toast.LENGTH_SHORT).show();
     }
 }
